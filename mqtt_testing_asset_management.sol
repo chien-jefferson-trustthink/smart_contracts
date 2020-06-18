e1a38b0f8150417ec37bdf59c8a2f4cb9e426e8d
chien-jefferson-trustthink <66162968+chien-jefferson-trustthink@users.noreply.github.com>
2020-06-17 23:58:00 -0700

    Testing solidity file for MQTT

---
+++ mqtt_testing_asset_management.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.10;
+
+contract asset_management{
+    uint public item_counter = 0;
+    Item[] public items;
+    
+    struct Item{
+        string serial_number;
+        string asset_type;
+        uint date; //mmddyyyy
+        uint time; //hhmmss
+        string manufacturer;
+    }
+    
+    function addItem(string memory _serial_number, string memory _asset_type, uint _date, 
+        uint _time, string memory _manufacturer) public {
+        items.push(Item(_serial_number, _asset_type, _date, _time, _manufacturer));
+        item_counter++;
+    }
+}
